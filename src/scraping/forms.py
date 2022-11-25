82d7d899dea168dc5bd36dab65dd926d06cbe3d5
Yusif1989 <o.yusif@hotmail.com>
2022-11-25 23:10:42 +0300

    Add registration

--- src/scraping/forms.py
+++ src/scraping/forms.py
@@ -3,5 +3,13 @@ from django import forms
 from scraping.models import City, Language
 
 class FindForm(forms.Form):
-    city = forms.ModelChoiceField(queryset = City.objects.all())
-    language = forms.ModelChoiceField(queryset = Language.objects.all())
+    #If we add like a field name slug , then we need to change that field for home viev function in view.py
+    # requered = if field empty also get request in form
+    # Widgets - helps us to change default view of form fields, in our example we use bootstrap form-control class
+    # Use label for change name of inputs
+    city = forms.ModelChoiceField(queryset = City.objects.all(), to_field_name="slug", required=False, 
+    widget=forms.Select(attrs={'class': 'form-control'}), label='City'
+    )
+    language = forms.ModelChoiceField(queryset = Language.objects.all(), to_field_name="slug", required=False,
+    widget=forms.Select(attrs={'class': 'form-control'}), label='Major'
+    )
--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from scraping.forms import FindForm
 
 from scraping.models import Vacancy
 
 def home_view(request):
     # for show post request in cmd we use this command
-    print(request.GET) # ONLY IN CMD 
+    #print(request.GET) # ONLY IN CMD 
+    # ADD OUR TO HOME PAGE REQUEST
+    form = FindForm()
     # ADD TO THE VARIABLE GET REQUEST IN FORMS
     city = request.GET.get('city')
     language = request.GET.get('language')
@@ -16,14 +19,14 @@ def home_view(request):
         # Create an empty dictioanary
         _filter = {}
         # If user add city name add to the filter 
-        # we use 2 under line(__) between to call the field name -- city__name => model.name__fieldName
+        # we use 2 under line(__) between to call the field name -- city__slug => model.name__fieldName
         if city:
-            _filter['city__name'] = city
+            _filter['city__slug'] = city
         # If user add city name add to the filter 
         if language:
-            _filter['language__name'] = language
+            _filter['language__slug'] = language
     #call our vacancy list
     # ** ==> Returns a new QuerySet containing objects that match the given lookup parameters.
         qs = Vacancy.objects.filter(**_filter)
     # We transmit our vacancy to render like an deictionary
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
